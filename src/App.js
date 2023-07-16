313960eaa951df5d6420586569c78f1cfa6e5450
Pankaj <pankz9@gmail.com>
2023-07-16 15:27:44 +0100

    Setup map

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component }  from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { Icon } from 'leaflet'
+const myIcon = new Icon({
+  iconUrl: "https://unpkg.com/leaflet@1.9.3/dist/images/marker-icon.png",
+ iconSize: [20,32]
+})
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <MapContainer
+        center={[51.505, -0.09]}  // Initial position latitude and longitude
+        zoom={12}                  // Initial zoom level
+        style={{ height: '600px', width: '100%' }}
+      >
+        {/* Map layers go here */}
+        
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution="© OpenStreetMap contributors"
+        />
+
+      <Marker position={[51.505, -0.09]} icon={myIcon}>
+      <Popup>
+      Coffee Shop!
+      </Popup>
+      </Marker> 
+      </MapContainer>
+    );
+
 }
 
 export default App;
